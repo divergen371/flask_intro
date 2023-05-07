f8d3dbb56277b8c375ab52f1f72485aa422eb2e0
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-05-07 21:17:32 +0900

    ログイン画面にルーティング

--- app.py
+++ app.py
@@ -7,3 +7,13 @@ app = Flask(__name__)
 key = os.urandom(21)
 app.secret_key = key
 id_pwd = {"hoge": "hogehoge"}
+
+
+@app.route("/")
+def index():
+    return "Hello Flask"
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
