0136376c51c71f4ba2dbeebad32c7fafb696ad57
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-05-07 21:33:39 +0900

    認証機能を追加した

--- app.py
+++ app.py
@@ -11,9 +11,28 @@ id_pwd = {"hoge": "hogehoge"}
 
 @app.route("/")
 def index():
-    return "Hello Flask"
+    if not session.get("login"):
+        return redirect(url_for("login"))
+    else:
+        return render_template("index.html")
 
 
 @app.route("/login")
 def login():
     return render_template("login.html")
+
+
+@app.route("/logincheck", methods=["POST"])
+def logincheck():
+    user_id = request.form["user_id"]
+    password = request.form["password"]
+
+    if (user_id in id_pwd) and (password == id_pwd[user_id]):
+        session["login"] = True
+    else:
+        session["login"] = False
+
+    if session["login"]:
+        return redirect(url_for("index"))
+    else:
+        return redirect(url_for("login"))
